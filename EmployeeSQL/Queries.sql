63458cf15292e27bf52059331faf124fb67a5f3b
Matthew <ashlander@live.com>
2020-11-21 14:44:00 -0600

    Started optional

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -6,28 +6,29 @@ employees.emp_no=salaries.emp_no;
 select firstname, lastname, hire_date from employees where hire_date between '1/1/1986' and '12/31/1986';
 
 select departments.dept_no, departments.dept_name, managers.man_no, employees.lastname, employees.firstname
-from ((departments
-inner join managers on departments.dept_no = managers.dept_no)
-inner join employees on managers.man_no = employees.emp_no);
+from departments
+inner join managers on departments.dept_no = managers.dept_no
+inner join employees on managers.man_no = employees.emp_no;
 
 select employees.emp_no, employees.lastname, employees.firstname, departments.dept_name
-from ((employees
-inner join dept_emp on employees.emp_no = dept_emp.emp_no)
-inner join departments on dept_emp.dept_no = departments.dept_no);
+from employees
+inner join dept_emp on employees.emp_no = dept_emp.emp_no
+inner join departments on dept_emp.dept_no = departments.dept_no;
 
 select firstname, lastname, sex
 from employees
 where firstname = 'Hercules' and lastname like 'B%';
 
 select employees.emp_no, employees.lastname, employees.firstname, departments.dept_name
-from ((employees
-inner join dept_emp on employees.emp_no = dept_emp.emp_no)
-inner join departments on dept_emp.dept_no = departments.dept_no);
+from employees
+inner join dept_emp on employees.emp_no = dept_emp.emp_no
+inner join departments on dept_emp.dept_no = departments.dept_no
+where departments.dept_name = 'Sales';
 
 select employees.emp_no, employees.lastname, employees.firstname, departments.dept_name
-from ((employees
-inner join dept_emp on employees.emp_no = dept_emp.emp_no)
-inner join departments on dept_emp.dept_no = departments.dept_no)
+from employees
+inner join dept_emp on employees.emp_no = dept_emp.emp_no
+inner join departments on dept_emp.dept_no = departments.dept_no
 where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
 
 select lastname, count(lastname)
